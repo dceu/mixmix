ff6bdc713838a82cbf573a58fea6ad3974db7b10
Donovan Conrad E. Uy <dceu00@gmail.com>
2019-08-04 15:04:46 -0400

    ingredient controller

--- src/main/java/com/donovanuy/mixmix/controllers/IngredientController.java
+++ src/main/java/com/donovanuy/mixmix/controllers/IngredientController.java
@@ -1,12 +1,40 @@
 package com.donovanuy.mixmix.controllers;
 
+import java.util.Map;
+
+import com.donovanuy.mixmix.repository.IngredientRepository;
+
+import javax.persistence.Id;
+
+
+import com.donovanuy.mixmix.entities.Ingredient;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping(path="/pantry")
 public class IngredientController {
+    
+    @Autowired
+    IngredientRepository ingredientRepository;
 
-    @GetMapping("/ingredient")
-    public String ingredient(@RequestParam(
-            
-    ))
+    @PostMapping("/ingredient")
+    public Ingredient create(@RequestBody Map<String, String> body){
+        String name = body.get("name");
+        String description = body.get("description");
+        return ingredientRepository.save(new Ingredient(name, description));
+    }
+    
+
+    @GetMapping("/ingredient/{id}")
+    public Ingredient show(@PathVariable String id){
+        
+        int ingredId = Integer.parseInt(id);
+        return ingredientRepository.getOne(ingredId);
+    }
+
+    
 }
