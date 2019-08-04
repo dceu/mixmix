e6b1eb8de11d35c78f167d971645c153607c7b1d
Donovan Conrad E. Uy <dceu00@gmail.com>
2019-08-04 15:05:52 -0400

    UserRepository

--- src/main/java/com/donovanuy/mixmix/repository/UserRepository.java
+++ src/main/java/com/donovanuy/mixmix/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.donovanuy.mixmix.repository;
 
-import org.springframework.data.repository.CrudRepository;
+//import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.donovanuy.mixmix.entities.User;
+import java.util.List;
 
-public interface UserRepository extends CrudRepository<User, Integer> {
-
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    List<User> findAllById(Integer id);
 }
