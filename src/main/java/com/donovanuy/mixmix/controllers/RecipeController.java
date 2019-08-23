8b184aaa5fb99d77ef65246d1b90e1701b48b8c7
Donovan Conrad E. Uy <dceu00@gmail.com>
2019-08-22 22:31:16 -0400

    feat: allow json view of entities

--- src/main/java/com/donovanuy/mixmix/controllers/RecipeController.java
+++ src/main/java/com/donovanuy/mixmix/controllers/RecipeController.java
@@ -1,20 +1,55 @@
 package com.donovanuy.mixmix.controllers;
 
+
+import java.util.*;
+
+import com.donovanuy.mixmix.managers.*;
+import com.donovanuy.mixmix.entities.*;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 @Controller
+@RequestMapping("/recipes")
 public class RecipeController {
+
+    @Autowired
+    private RecipeMan recipeMan;
+
+    @GetMapping("/all")
+    public String viewAllRecipes(Model model){
+        List<Recipe> listRecipes = recipeMan.listAll();
+        Iterator<Recipe> itr = listRecipes.iterator();
+        while (itr.hasNext()){
+            Recipe r = itr.next();
+            if (r.getComplete().equals("FALSE")){
+                itr.remove();
+            }
+        }
+        model.addAttribute("listRecipes", listRecipes);
+        return "recipe-index";
+    }
+
     
-    @GetMapping("/recipe")
-    public String recipe(@RequestParam(
-                            name = "name",
-                            required = false,
-                            defaultValue= "Untitled")
-                        String name, Model model)
-                            {model.addAttribute("name", name);
-                        return "recipe";
-                    }
+    
+    @GetMapping("/allJSON")
+    public @ResponseBody Iterable<Recipe> getAllRecipess() {
+        return recipeMan.listAll();
+        
+        // for(Recipe r: listRecipes){
+        //     System.out.println(r.getName());
+        //     if (r.getComplete().equals("FALSE")){
+        //     listRecipes.remove(r);
+        //     System.out.println("removed "+ r.getName() + " from viewList" );
+        //     }
+        // }
+
+        // model.addAttribute("listRecipes", listRecipes);
+
+        // return "recipe-index";
+    }
     
 }
