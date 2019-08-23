0eb3423d2f8612c90cdb41b966c21159b2d07740
Donovan Conrad E. Uy <dceu00@gmail.com>
2019-08-22 22:29:40 -0400

    fix: restful api patterns

--- src/main/java/com/donovanuy/mixmix/controllers/IngredientController.java
+++ src/main/java/com/donovanuy/mixmix/controllers/IngredientController.java
@@ -1,6 +1,6 @@
 package com.donovanuy.mixmix.controllers;
 
-import java.util.Map;
+import java.util.*;
 
 import com.donovanuy.mixmix.repository.IngredientRepository;
 
@@ -11,29 +11,48 @@ import com.donovanuy.mixmix.entities.Ingredient;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping(path="/pantry")
+@RequestMapping(path="/ingredients")
 public class IngredientController {
     
     @Autowired
     IngredientRepository ingredientRepository;
 
-    @PostMapping("/ingredient")
-    public Ingredient create(@RequestBody Map<String, String> body){
-        String name = body.get("name");
-        String description = body.get("description");
-        return ingredientRepository.save(new Ingredient(name, description));
+    @GetMapping("/all")
+    public String viewPantry(Model model){
+        List<Ingredient> listIngredients = ingredientRepository.findAll();
+        model.addAttribute("listIngredients", listIngredients);
+        return "pantry";
     }
-    
+
+    // @PostMapping("/ingredient")
+    // public Ingredient create(@RequestBody Map<String, String> body){
+    //     String name = body.get("name");
+    //     String description = body.get("description");
+    //     return ingredientRepository.save(new Ingredient(name, description));
+    // }
+    @RequestMapping("/ingredient/new")
+    public String createNewIngredient(Model model){
+        Ingredient ingredient = new Ingredient();
+        model.addAttribute("ingredient", ingredient);
+        return "new-ingredient";
+    }
+
+    @RequestMapping(value = "/ingredient/new/save", method = RequestMethod.POST)
+    public String saveIngredient(@ModelAttribute("ingredient") Ingredient ingredient){
+        ingredientRepository.save(ingredient);
+        return "redirect:/ingredients/all";
+    }
 
     @GetMapping("/ingredient/{id}")
     public Ingredient show(@PathVariable String id){
         
-        int ingredId = Integer.parseInt(id);
-        return ingredientRepository.getOne(ingredId);
+        Integer ingredId = Integer.parseInt(id);
+        Long l = ingredId.longValue();
+        return ingredientRepository.getOne(l);
     }
 
     
