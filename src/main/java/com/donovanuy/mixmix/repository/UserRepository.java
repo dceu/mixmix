c092142dfbf998bd062d3d9581307f45960625c0
Donovan Conrad E. Uy <dceu00@gmail.com>
2019-08-22 22:37:01 -0400

    User repo as JPA Repo

--- src/main/java/com/donovanuy/mixmix/repository/UserRepository.java
+++ src/main/java/com/donovanuy/mixmix/repository/UserRepository.java
@@ -2,12 +2,12 @@ package com.donovanuy.mixmix.repository;
 
 //import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 import com.donovanuy.mixmix.entities.User;
 import java.util.List;
 
-@Repository
+
 public interface UserRepository extends JpaRepository<User, Integer> {
     List<User> findAllById(Integer id);
 }
