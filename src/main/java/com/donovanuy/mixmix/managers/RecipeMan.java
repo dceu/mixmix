85f44c61905c8ddf0b23998132d39ba4260c22fb
Donovan Conrad E. Uy <dceu00@gmail.com>
2019-08-22 22:35:33 -0400

    @Autowired service class

--- src/main/java/com/donovanuy/mixmix/managers/RecipeMan.java
+++ src/main/java/com/donovanuy/mixmix/managers/RecipeMan.java
@@ -1,15 +1,39 @@
 package com.donovanuy.mixmix.managers;
 
+import java.util.*;
+
 import com.donovanuy.mixmix.entities.*;
+import com.donovanuy.mixmix.repository.RecipeRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
 public class RecipeMan{
-    RecipeMan(){}
+    
 
-    private RecipeMan instance = new RecipeMan();
 
-    public RecipeMan getInstance(){
-        return instance;
+    @Autowired
+    private RecipeRepository repo;
+
+    public List<Recipe> listAll() {
+        return repo.findAll();
     }
+
+    public void save(Recipe recipe){
+        repo.save(recipe);
+    }
+
+    public Recipe get(String s){
+        return repo.findById(s).get();
+    }
+
+    public void delete(String s){
+        repo.deleteById(s);
+    }
+
 
     // private Recipe newTitledRecipe(String s){
     //     return new Recipe(s);
