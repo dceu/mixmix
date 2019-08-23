4b9cd95d1e26eb54aca2c8180d7316a378e2a84c
Donovan Conrad E. Uy <dceu00@gmail.com>
2019-08-22 22:36:10 -0400

    Long ID

--- src/main/java/com/donovanuy/mixmix/repository/IngredientRepository.java
+++ src/main/java/com/donovanuy/mixmix/repository/IngredientRepository.java
@@ -2,12 +2,12 @@ package com.donovanuy.mixmix.repository;
 
 //import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 import com.donovanuy.mixmix.entities.Ingredient;
 import java.util.List;
 
-@Repository
-public interface IngredientRepository extends JpaRepository<Ingredient, Integer> {
-    List<Ingredient> findAllById(Integer id);
+
+public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
+    List<Ingredient> findAllById(Long id);
 }
