9d280ee1c16267848016e6d80a6110639dc728c4
Donovan Conrad E. Uy <dceu00@gmail.com>
2019-08-28 22:18:30 -0400

    feat: userRecipe logic

--- src/main/java/com/donovanuy/mixmix/controllers/RecipeController.java
+++ src/main/java/com/donovanuy/mixmix/controllers/RecipeController.java
@@ -4,12 +4,27 @@ package com.donovanuy.mixmix.controllers;
 import java.util.*;
 
 import com.donovanuy.mixmix.managers.*;
+import com.donovanuy.mixmix.repository.IngredientRepository;
+import com.donovanuy.mixmix.repository.UserRecipeRepo;
 import com.donovanuy.mixmix.entities.*;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import net.bytebuddy.implementation.bind.MethodDelegationBinder.BindingResolver;
+
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+
+import javax.validation.Valid;
 
 
 @Controller
@@ -19,7 +34,14 @@ public class RecipeController {
     @Autowired
     private RecipeMan recipeMan;
 
-    @GetMapping("/all")
+    @Autowired
+    private UserRecipeRepo userRecipeRepo;
+
+    @Autowired
+    private IngredientRepository ingredientRepo;
+
+
+    @GetMapping("/")
     public String viewAllRecipes(Model model){
         List<Recipe> listRecipes = recipeMan.listAll();
         Iterator<Recipe> itr = listRecipes.iterator();
@@ -33,8 +55,6 @@ public class RecipeController {
         return "recipe-index";
     }
 
-    
-    
     @GetMapping("/allJSON")
     public @ResponseBody Iterable<Recipe> getAllRecipess() {
         return recipeMan.listAll();
@@ -52,4 +72,68 @@ public class RecipeController {
         // return "recipe-index";
     }
     
+
+    //User Recipe Actions
+
+    @GetMapping("/userRecipes")
+    public Page<UserRecipe> getAllUserRecipes(Pageable pageable){
+        return recipeMan.listAllUserRecipes(pageable);
+    }
+
+    @PostMapping("/userRecipes")
+    public UserRecipe createRecipe(@Valid @RequestBody UserRecipe ur){
+        return recipeMan.saveUserRecipe(ur);
+    }
+
+    @RequestMapping("/userRecipes/new")
+    public String createNewUserRecipe(Model model){
+        UserRecipe userRecipe = new UserRecipe();
+        model.addAttribute("userRecipe", userRecipe);
+        return "new-recipe";
+    }
+
+    @RequestMapping(value = "/userRecipes/new/save", method = RequestMethod.POST)
+    public String saveRecipe(
+        @Valid
+        @RequestBody UserRecipe userRecipe,
+        @RequestParam(value = "sel_ingredients", required = false) long[] sel_ingredients,
+        BindingResult bindingResult, Model model){
+        // for(long l : sel_ingredients){
+        //     Ingredient ing = new Ingredient();
+        //     ing = ingredientRepo.getOne(l);
+        //     userRecipe.addIngredient(ing);
+        // }
+        userRecipeRepo.save(userRecipe);
+        return "redirect:../";
+    }
+
+
+
+    @ModelAttribute
+    public void populateIngredients(Model model){
+        List<Ingredient> ingredientList = this.ingredientRepo.findAll();
+        //Iterator<Ingredient> itr = ingredientList.iterator();
+        model.addAttribute("allIngredients", ingredientList);
+        
+    }
+
+    @PutMapping("/userRecipes/{userRecipeId}")
+    public UserRecipe updateUserRecipe(@PathVariable Long userRecipeId, @Valid @RequestBody UserRecipe postRequest){
+        return userRecipeRepo.findById(userRecipeId).map( userRecipe -> {
+            userRecipe.setTitle(postRequest.getTitle());
+            userRecipe.setDescription(postRequest.getDescription());
+            userRecipe.setIngredients(postRequest.getIngredients());
+            userRecipe.setPreparation(postRequest.getPreparation());
+            return userRecipeRepo.save(userRecipe);
+        }).orElseThrow(() -> new ResourceNotFoundException("UserRecipeId " + userRecipeId + " not found"));  
+    }
+
+    @DeleteMapping("/userRecipes/{userRecipeId}")
+    public ResponseEntity<?> deleteRecipe(@PathVariable Long userRecipeId){
+        return userRecipeRepo.findById(userRecipeId).map(userRecipe -> {
+            userRecipeRepo.delete(userRecipe);
+            return ResponseEntity.ok().build();
+       }).orElseThrow(() -> new ResourceNotFoundException("UserRecipeId " + userRecipeId + " not found"));
+    }
+
 }
