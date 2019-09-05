c9d18c85a2dbfe66d4eb1bf5a48a29e806a4d5a3
Donovan Conrad E. Uy <dceu00@gmail.com>
2019-09-04 23:25:21 -0400

    new feat: roles, for security

---
+++ src/main/java/com/donovanuy/mixmix/entities/Role.java
@@ -0,0 +1,44 @@
+package com.donovanuy.mixmix.entities;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "role" )
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+    //Setters & Getters
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<User> getUsers() {
+        return this.users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+
+}
