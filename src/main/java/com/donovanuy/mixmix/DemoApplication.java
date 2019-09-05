b3b0dd10c6ca3d87bfd1d3af6f15190a8c25cea3
Donovan Conrad E. Uy <dceu00@gmail.com>
2019-09-04 23:24:10 -0400

    spring security

--- src/main/java/com/donovanuy/mixmix/DemoApplication.java
+++ src/main/java/com/donovanuy/mixmix/DemoApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+// import org.springframework.boot.builder.SpringApplicationBuilder;
 
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class DemoApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
