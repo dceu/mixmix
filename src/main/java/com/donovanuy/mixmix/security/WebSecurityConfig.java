d4dc42ea99e6d8de9eaeba26a70b5055924ceccf
Donovan Conrad E. Uy <dceu00@gmail.com>
2019-09-04 23:26:37 -0400

    enable spring security

--- src/main/java/com/donovanuy/mixmix/security/WebSecurityConfig.java
+++ src/main/java/com/donovanuy/mixmix/security/WebSecurityConfig.java
@@ -1,43 +1,55 @@
-// package com.donovanuy.mixmix.security;
+package com.donovanuy.mixmix.security;
 
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-// import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-// import org.springframework.security.core.userdetails.User;
-// import org.springframework.security.core.userdetails.UserDetails;
-// import org.springframework.security.core.userdetails.UserDetailsService;
-// import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-// @Configuration
-// @EnableWebSecurity
-// public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-//     @Override
-//     protected void configure(HttpSecurity http) throws Exception {
-//         http
-//             .authorizeRequests()
-//                 .antMatchers("/", "/home","/ingredients").permitAll()
-//                 .anyRequest().authenticated()
-//                 .and()
-//             .formLogin()
-//                 .loginPage("/login")
-//                 .permitAll()
-//                 .and()
-//             .logout()
-//                 .permitAll();
-//     }
 
-//     @Bean
-//     @Override
-//     public UserDetailsService userDetailsService() {
-//         UserDetails user =
-//              User.withDefaultPasswordEncoder()
-//                 .username("user")
-//                 .password("password")
-//                 .roles("USER")
-//                 .build();
 
-//         return new InMemoryUserDetailsManager(user);
-//     }
-// }
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+@Autowired
+private UserDetailsService userDetailsService;
+
+
+
+@Bean
+public BCryptPasswordEncoder bCryptPasswordEncoder() {
+    return new BCryptPasswordEncoder();
+}
+
+
+@Bean
+public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
+}
+
+@Override
+protected void configure(HttpSecurity http) throws Exception {
+    http
+            .authorizeRequests()
+                .antMatchers("/resources/**", "/registration").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+            .logout()
+                .permitAll();
+}
+
+@Autowired
+public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+}
+}
