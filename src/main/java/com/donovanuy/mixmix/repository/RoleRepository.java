7b901db1f6b6fae62adcd19cd6c80958d2525c09
Donovan Conrad E. Uy <dceu00@gmail.com>
2019-09-04 23:26:03 -0400

    spring security

---
+++ src/main/java/com/donovanuy/mixmix/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.donovanuy.mixmix.repository;
+
+import com.donovanuy.mixmix.entities.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long>{
+    
+}
