110520c69cdb6900f4ab330abccf4e8bd471eba6
Donovan Conrad E. Uy <dceu00@gmail.com>
2019-09-04 23:25:50 -0400

    refactor: user class for spring security

--- src/main/java/com/donovanuy/mixmix/entities/User.java
+++ src/main/java/com/donovanuy/mixmix/entities/User.java
@@ -1,38 +1,119 @@
 package com.donovanuy.mixmix.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.Email;
+import java.util.*;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import javax.persistence.*;
+
+import javax.validation.constraints.*;
 
 
+@Entity
+@Table(name = "users")
+public class User {
 
-@Entity // tells Hibernate to make a table out of this class
+    // User Fields
 
-public class User{
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    private Integer id;
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
 
     @NotNull
     @Size(min=2, max=30)
-    private String name;
+    private String username;
 
     @NotNull
     @Min(21)
     private Integer age;
 
+    @Column(name="email", unique= true)
     @NotNull
     @Email
     private String email;
 
+    private String password;
+
+    @Transient
+    private String passwordConfirm;
+
+    @ManyToMany
+    @JoinTable(name = "user_role",
+                joinColumns = @JoinColumn(name = "user_id"),
+                inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
 
     
-    // setters & getters
+    @Column(name = "createdRecipes")
+    UserRecipe createdRecipes;
+
+   
+    @Column(name ="favorited")
+    UserRecipe favorited;
+
+    // setters and getters
+
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPasswordConfirm() {
+        return this.passwordConfirm;
+    }
+
+    public void setPasswordConfirm(String passwordConfirm) {
+        this.passwordConfirm = passwordConfirm;
+    }
+
+    public Set<Role> getRoles() {
+        return this.roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+
+    public UserRecipe getCreatedRecipes() {
+        return this.createdRecipes;
+    }
+
+    public void setCreatedRecipes(UserRecipe createdRecipes) {
+        this.createdRecipes = createdRecipes;
+    }
+
+    public UserRecipe getFavorited() {
+        return this.favorited;
+    }
+
+    public void setFavorited(UserRecipe favorited) {
+        this.favorited = favorited;
+    }
+
+    
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
     public Integer getAge() {
         return this.age;
@@ -40,24 +121,6 @@ public class User{
 
     public void setAge(Integer age) {
         this.age = age;
-    }
-
-    
-
-    public Integer getId() {
-        return this.id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getEmail() {
@@ -68,4 +131,5 @@ public class User{
         this.email = email;
     }
 
+    
 }
